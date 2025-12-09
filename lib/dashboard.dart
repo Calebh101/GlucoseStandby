ed6a0c3dd029e583af63e5841a6f7f33773f2ba3
Caleb <calebh101dev@icloud.com>
2025-12-09 08:17:06 -0600

    Start building architecture

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:dexcom/dexcom.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
@@ -8,6 +9,31 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  late Dexcom dexcom;
+  late DexcomStreamProvider provider;
+  (DexcomReading?, DexcomReading?)? readings; // Latest, next latest
+
+  Future<void> reloadSettings() async {}
+
+  @override
+  void initState() {
+    dexcom = Dexcom();
+    provider = DexcomStreamProvider(dexcom);
+
+    provider.listen(onData: (data) {
+      readings = (data.elementAtOrNull(0), data.elementAtOrNull(1));
+      reloadSettings();
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    provider.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
